876ab30adf293e15ae49c2e2a3183c460a171fc2
André Lima <allagyn@gmail.com>
2022-05-21 18:05:39 -0300

    Pasta Iniciante novo arquivo soma_dois_numeros.py

---
+++ iniciante/soma_dois_numeros.py
@@ -0,0 +1,5 @@
+# Faça um programa que peça dois números inteiros e imprima a soma desses dois números
+n1 = int(input('Digite o 1º valor:'))
+n2 = int(input('Digite o 2º Valor:'))
+s = n1+n2
+print(f'A soma de {n1}+{n2} é:{s}')
